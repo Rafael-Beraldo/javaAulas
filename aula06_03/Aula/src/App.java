5315ee1f74a97a2a504f6c75a2d2f6ede1162c7a
Rafael Beraldo <raffatemplate@gmail.com>
2023-03-06 22:17:52 -0300

    Aula dia 06/03/2023

---
+++ aula06_03/Aula/src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner read = new Scanner(System.in);
+        
+        /*//Comparação de Strings
+        String n1 = "Ana",n2 = "Ana";
+        System.out.println("Igualdade "+n1.equals(n2)); // Igualdade entre Strings
+        System.out.println("Desigualdade "+!n1.equals(n2));
+        */
+    }
+}
--- operadores/operadoresAritmeticos/src/App.java
+++ operadores/operadoresAritmeticos/src/App.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) throws Exception {
         /* Criar Algoritmo de Média de Aluno Simples */
@@ -15,19 +17,22 @@ public class App {
         n2 = read.nextFloat();
         media = (n1+n2) / 2;
         
-            if((n1+n2) > 20){
+            if((n1+n2) > 20 || (n1+n2) < 0){
                 System.out.println("ERRO! A soma das Notas está maior que o Normal! \n");
                 System.out.println("INSIRA NOTAS NOVAMENTE!");
             }else{
                 System.out.println("NOTAS ANEXADAS COM SUCESSO! \n");
             }
-        }while((n1+n2) > 20);
+        }while((n1+n2) > 20 || (n1+n2) < 0);
         
-        if(media < 6){
-            System.out.println("REPROVADO! \n");
+        if(media >= 6){
+            System.out.println("APROVADO! \n");
+        }else if(media >= 4){
+            System.out.println("NÃO ATINGIU A MÉDIA, FAÇA OUTRO EXAME!");
         }else{
-            System.out.println("PARABÉNS "+nome+"! APROVADO! \n");
+            System.out.println("REPROVADO!");
         }
+
         System.out.println("| Nome : "+nome+" |");
         System.out.println("| 1° Nota : "+n1+" |");
         System.out.println("| 2° Nota : "+n2+" |");
